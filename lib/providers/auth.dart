f63a05caeaf1c15695aec929db676bd3e3dc2af7
AbylaikhanZ <abylaikhan.zhumagazy@gmail.com>
2023-02-04 16:11:12 +0600

    added a sign in and error handling for auth

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -4,18 +4,39 @@ import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
+import 'package:shop_app/models/http_exception.dart';
 
 class Auth with ChangeNotifier {
   String _token;
   DateTime _expiryDate;
   String _userId;
 
-  Future<void> signup(String email, String password) async {
+  Future<void> _authenticate(
+      String email, String password, String urlType) async {
     final url = Uri.parse(
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA_T-UJA84_qv5KJQW56l6-04NTn4bZfIw");
+        "https://identitytoolkit.googleapis.com/v1/accounts:$urlType?key=AIzaSyA_T-UJA84_qv5KJQW56l6-04NTn4bZfIw");
     // ignore: unused_local_variable
-    final response = await http.post(url,
-        body: json.encode(
-            {"email": email, "password": password, "returnSecureToken": true}));
+    try {
+      final response = await http.post(url,
+          body: json.encode({
+            "email": email,
+            "password": password,
+            "returnSecureToken": true
+          }));
+      final responseData = json.decode(response.body);
+      if (responseData["error"] != null) {
+        throw HttpException(responseData["error"]["message"]);
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, "signUp");
+  }
+
+  Future<void> signin(String email, String password) async {
+    return _authenticate(email, password, "signInWithPassword");
   }
 }
